fc9f955506d4be72f84b1ae1a0633ed782eedaa8
Ahmet-Special <76907366+Ahmet-Special@users.noreply.github.com>
2021-01-06 10:28:09 +0300

    Add files via upload

---
+++ void.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+termux-setup-storage
+apt-get update -y
+apt-get upgrade -y
+pkg install git -y
+pkg install curl -y
+pkg install wget -y 
+clear
+echo "Void Was established 
+curl https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Void/void.sh | bash 
+clear 
+echo "Void Was established "
